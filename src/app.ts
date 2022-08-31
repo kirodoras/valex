b58b304cbef3ff2543ff88f9c06943ed53bf0d0e
Mateus Figueiredo Pereira <figueiredo1497@gmail.com>
2022-08-31 18:04:44 -0300

    feat: error handler

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,8 @@
-import express, { json } from 'express';
-import cors from 'cors';
-import dotenv from 'dotenv';
-import router from './routes/index.js';
+import express, { json } from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import router from "./routes/index.js";
+import { errorHandler } from "./middlewares/errorHandler.middleware.js";
 
 dotenv.config();
 const app = express();
@@ -9,9 +10,10 @@ const app = express();
 app.use(json());
 app.use(cors());
 app.use(router);
+app.use(errorHandler);
 
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
-    console.log(`Listening to port ${port}`);
-})
+  console.log(`Listening to port ${port}`);
+});
